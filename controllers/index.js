ad69560b851251bd8ce62a50ff3801087edfe554
Almar <almar.asetbekov@gmail.com>
2023-04-30 22:07:47 +0200

    Add update and delte routes

--- controllers/index.js
+++ controllers/index.js
@@ -8,12 +8,32 @@ const get_all_todos = asyncHandler(async (req, res) => {
 
 const create_todo = asyncHandler(async (req, res) => {
     const { description } = req.body;
-    const todo = await new Todo({ description });
+    const todo = new Todo({ description });
     await todo.save();
     res.sendStatus(201);
+});
+
+const update_todo = asyncHandler(async (req, res) => {
+    const { id, description } = req.body;
+    const updatedTodo = await Todo.findOneAndUpdate({ id }, { $set: { description } });
+    if (updatedTodo) {
+        res.sendStatus(200);
+    }
+    res.status(404).send('Todo is not found.');
+});
+
+const delete_todo = asyncHandler(async (req, res) => {
+    const { id } = req.body;
+    const deletedTodo = await Todo.findOneAndDelete({ id });
+    if (deletedTodo) {
+        res.sendStatus(200);
+    }
+    res.status(404).send('Todo is not found.');
 });
 
 module.exports = {
     get_all_todos,
     create_todo,
+    update_todo,
+    delete_todo,
 };
