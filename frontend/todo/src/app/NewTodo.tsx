e0813641133caf03abb5f9eb3965f85ce87ece9d
Almar <almar.asetbekov@gmail.com>
2023-08-24 19:29:21 +0200

    feature: Add update and delete to todos

--- frontend/todo/src/app/NewTodo.tsx
+++ frontend/todo/src/app/NewTodo.tsx
@@ -2,10 +2,15 @@ import axios from 'axios';
 import { useRef } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 
-const createTodo = async ({ description }: { description: string }) => {
+type Todo = {
+    description: string;
+    deadline: string;
+};
+
+const createTodo = async ({ description, deadline }: Todo) => {
     const { data } = await axios.post('http://localhost:4000/create', {
         description,
-        deadline: new Date().toISOString(),
+        deadline: new Date(deadline).toISOString(),
     });
     return data;
 };
@@ -23,18 +28,22 @@ export const NewTodo = () => {
 
     const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (!inputRef.current?.value.trim()) return;
-        console.log(dateRef.current?.value);
+        if (!inputRef.current?.value.trim() || !dateRef.current?.value) return;
 
-        // await mutation.mutateAsync({ description: inputRef.current?.value });
+        try {
+            await mutation.mutateAsync({ description: inputRef.current?.value, deadline: dateRef.current?.value });
+            inputRef.current.value = '';
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     return (
         <form onSubmit={submitHandler} className={'flex flex-col text-xl'}>
             <label htmlFor={'description'}>Description</label>
-            <input type={'text'} ref={inputRef} />
+            <input type={'text'} ref={inputRef} className={'text-black'} />
             <label htmlFor={'date'}>Deadline</label>
-            <input type={'date'} ref={dateRef} defaultValue={new Date().toISOString().slice(0, 10)} />
+            <input type={'date'} ref={dateRef} defaultValue={new Date().toISOString().slice(0, 10)} className={'text-black'} />
             <button type={'submit'}>Add</button>
         </form>
     );
--- frontend/todo/src/app/Todo.tsx
+++ frontend/todo/src/app/Todo.tsx
@@ -1,8 +1,10 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import styles from './page.module.css';
 import axios from 'axios';
 import { NewTodo } from './NewTodo';
 import classnames from 'classnames';
+import { useEffect } from 'react';
+import React from 'react';
 
 type Todo = {
     _id: string;
@@ -10,6 +12,7 @@ type Todo = {
     createdAt: string;
     updatedAt: string;
     status: 'active' | 'done';
+    deadline: string;
 };
 
 type ApiResponse = {
@@ -21,16 +24,81 @@ const getTodos = async (): Promise<ApiResponse> => {
     return data;
 };
 
+const deleteTodo = async (id: string) => {
+    const { status } = await axios.delete(`http://localhost:4000/delete?id=${id}`);
+    return status === 200;
+};
+
+const updateTodo = async (todo: Todo) => {
+    const { _id, description, deadline, status } = todo;
+    const response = await axios.patch(`http://localhost:4000/update?id=${_id}`, {
+        description,
+        deadline,
+        status,
+    });
+
+    return response.status === 200;
+};
+
+interface TodoItemProps {
+    item: Todo;
+    updateHandler: (item: Todo, value: string) => void;
+    deleteHandler: (id: string) => void;
+}
+
+const TodoItem = ({ item, updateHandler, deleteHandler }: TodoItemProps) => {
+    const ref = React.useRef<HTMLInputElement>(null);
+
+    return (
+        <>
+            <input ref={ref} defaultValue={item.description} className={'bg-black'} />
+            <div>{item.deadline.slice(0, 10)}</div>
+            <button
+                className="rounded-full bg-sky-500/100 cursor-pointer"
+                onClick={() => updateHandler(item, ref?.current?.value || item.description)}
+            >
+                Update
+            </button>
+            <div className="cursor-pointer" onClick={() => deleteHandler(item._id)}>
+                X
+            </div>
+        </>
+    );
+};
+
 export const Todo = () => {
     const { data, isLoading, isError } = useQuery('todos', getTodos);
+    const queryClient = useQueryClient();
+
+    const updateHandler = async (todo: Todo, value: string) => {
+        todo.description = value;
+
+        const isUpdated = await updateTodo(todo);
+
+        if (isUpdated) {
+            // Invalidate and refetch the 'todos' query when the mutation is successful
+            queryClient.invalidateQueries('todos');
+        }
+    };
+
+    const deleteHandler = async (id: string) => {
+        const isDeleted = await deleteTodo(id);
+
+        if (isDeleted) {
+            // Invalidate and refetch the 'todos' query when the mutation is successful
+            queryClient.invalidateQueries('todos');
+        }
+    };
 
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error loading data</div>;
 
     return (
-        <main className={classnames(styles.main, 'text-2xl')}>
+        <main className={classnames(styles.main, 'text-2xl flex gap-5')}>
             {data?.todos.map((item) => (
-                <div key={item._id}>{item.description}</div>
+                <div key={item._id} className={'w-96 flex justify-between'}>
+                    <TodoItem item={item} updateHandler={updateHandler} deleteHandler={deleteHandler} />
+                </div>
             ))}
             <NewTodo />
         </main>
